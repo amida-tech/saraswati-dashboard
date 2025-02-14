9e3545943b1957cb3ed30dd44653202153b83919
jfetterolf <jonathan.fetterolf@amida.com>
2025-02-13 19:45:39 -0500

    fix compile-time errors

--- src/components/FilterMenu/FilterDrawerItem.js
+++ src/components/FilterMenu/FilterDrawerItem.js
@@ -1,80 +1,88 @@
 import {
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  Grid,
-  Tooltip,
-  Typography,
+    Checkbox,
+    FormControlLabel,
+    FormGroup,
+    Grid,
+    Tooltip,
+    Typography
 } from '@mui/material';
 import { useState } from 'react';
 import HelpIcon from '@mui/icons-material/Help';
 import PropTypes from 'prop-types';
 
 function FilterDrawerItem({ filterItem, currentFilter, filterAction }) {
-  const [defaultCheck] = useState(Array.from(currentFilter));
-  return (
-    <Grid container item className="filter-drawer-item">
-      <Grid container item className="filter-drawer-item__section">
-        <Grid item className="filter-drawer-item__row">
-          <Typography className="filter-drawer-item__label" variant="body1">
-            {filterItem.name}
-            :
-            {' '}
-          </Typography>
+    const [defaultCheck] = useState(Array.from(currentFilter));
+    // Instantiate options and values (prevents compile-time error: Cannot read properties of null)
+    const options = filterItem?.options ?? [];
+    const values = filterItem?.values ?? [];
+    return (
+        <Grid container item className='filter-drawer-item'>
+            <Grid container item className='filter-drawer-item__section'>
+                <Grid item className='filter-drawer-item__row'>
+                    <Typography
+                        className='filter-drawer-item__label'
+                        variant='body1'
+                    >
+                        {filterItem.name}:{' '}
+                    </Typography>
+                </Grid>
+                <Grid item className='filter-drawer-item__row'>
+                    <Tooltip title={filterItem.tip}>
+                        <HelpIcon className='filter-drawer-item__help-icon' />
+                    </Tooltip>
+                </Grid>
+            </Grid>
+            <FormGroup className='filter-drawer-item__options'>
+                {options.map((option, index) => (
+                    <FormControlLabel
+                        key={`filter-drawer-item-option-${option}`}
+                        componentsProps={{
+                            typography: {
+                                className: 'filter-drawer-item__option-label'
+                            }
+                        }}
+                        control={
+                            <Checkbox
+                                value={values[index] ?? ''}
+                                defaultChecked={defaultCheck.includes(
+                                    values[index]
+                                )}
+                                className='filter-drawer-item__option-checkbox'
+                                onChange={filterAction}
+                            />
+                        }
+                        label={option}
+                    />
+                ))}
+            </FormGroup>
         </Grid>
-        <Grid item className="filter-drawer-item__row">
-          <Tooltip title={filterItem.tip}>
-            <HelpIcon className="filter-drawer-item__help-icon" />
-          </Tooltip>
-        </Grid>
-      </Grid>
-      <FormGroup className="filter-drawer-item__options">
-        {filterItem?.options?.map((option, index) => (
-          <FormControlLabel
-            key={`filter-drawer-item-option-${option}`}
-            componentsProps={{
-              typography: { className: 'filter-drawer-item__option-label' },
-            }}
-            control={(
-              <Checkbox
-                value={filterItem.values[index]}
-                defaultChecked={defaultCheck.includes(filterItem.values[index])}
-                className="filter-drawer-item__option-checkbox"
-                onChange={filterAction}
-              />
-            )}
-            label={option}
-          />
-        ))}
-      </FormGroup>
-    </Grid>
-  );
+    );
 }
 
 FilterDrawerItem.propTypes = {
-  filterItem: PropTypes.shape({
-    name: PropTypes.string,
-    tip: PropTypes.string,
-    values: PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    filterItem: PropTypes.shape({
+        name: PropTypes.string,
+        tip: PropTypes.string,
+        values: PropTypes.arrayOf(
+            PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+        ),
+        options: PropTypes.arrayOf(PropTypes.string)
+    }),
+    currentFilter: PropTypes.arrayOf(
+        PropTypes.oneOfType([PropTypes.string, PropTypes.number])
     ),
-    options: PropTypes.arrayOf(PropTypes.string),
-  }),
-  currentFilter: PropTypes.arrayOf(
-    PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  ),
-  filterAction: PropTypes.func,
+    filterAction: PropTypes.func
 };
 
 FilterDrawerItem.defaultProps = {
-  filterItem: {
-    name: '',
-    tip: '',
-    options: [],
-    values: [],
-  },
-  currentFilter: [],
-  filterAction: undefined,
+    filterItem: {
+        name: '',
+        tip: '',
+        options: [],
+        values: []
+    },
+    currentFilter: [],
+    filterAction: undefined
 };
 
 export default FilterDrawerItem;
